55116a95f0d3d613f73e9a56d49c1931ba50a930
Alon J Adelson <contact@alonadelson.com>
2021-10-26 00:50:14 -0400

    setting state/converting to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import Web3 from 'web3'
 import Navbar from './Navbar'
 import './App.css'
 
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '0x0'
+ const App = (props) => {
+  const [state, setState] = useState(
+    { 
+      account: '0x0',
+      diaToken: {},
+      dappToken: {},
+      tokenFarm: {},
+      diaTokenBalance: '0',
+      dappTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
+    }
+  );
+  
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected please install the correct extension')
     }
   }
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                </a>
+  async function loadBlockchainData(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    setState(prevState => ({...prevState, account: accounts[0]}));
+  }
 
-                <h1>Hello, World!</h1>
+  useEffect(()=>{
+   loadWeb3();
+   loadBlockchainData(); 
+  },[]);
+  
+  return (
+    <div>
+          <Navbar account={state.account} />
+          <div className="container-fluid mt-5">
+            <div className="row">
+              <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+                <div className="content mr-auto ml-auto">
+                  <a
+                    href="http://www.dappuniversity.com/bootcamp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                  </a>
 
-              </div>
-            </main>
+                  <h1>Hello, World!</h1>
+
+                </div>
+              </main>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
   }
-}
 
 export default App;
